aba9198bb75d4a8d675fc0f84435e3a2d9c6966a
Asanobm <kim_bumin@fourglobe.co.jp>
2021-02-08 12:19:01 +0900

    Update main.py
    Multiple path and query parameters

--- main.py
+++ main.py
@@ -56,3 +56,14 @@ async def read_items(skip: int = 0, limit: int = 10):
     :return:
     """
     return items[skip: skip+limit]
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This is an amazing item ....."})
+    return item
