a575af0a1d13586f35e4746403336006fdda3500
Asanobm <kim_bumin@fourglobe.co.jp>
2021-02-08 15:53:25 +0900

    Create schemas.py
    Create the Pydantic models

---
+++ schemas.py
@@ -0,0 +1,33 @@
+from typing import Optional, List
+
+from pydantic import BaseModel
+
+
+class ItemBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    id: int
+    is_activate: bool
+    items: List[Item] = []
+
+    class Config:
+        orm_mode = True
